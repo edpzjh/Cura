6a842eb6820e8c32c8ee272c58e82a2255c9a612
Oday Maleh <oday.maleh@gmail.com>
2012-02-26 00:34:41 +0200

    Essential class

---
+++ src/com/cura/User.java
@@ -0,0 +1,95 @@
+/*
+ Copyright© 2010, 2011 Ahmad Balaa, Oday Maleh
+
+ This file is part of Cura.
+
+	Cura is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cura is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cura.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.cura;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class User implements Parcelable {
+	private String username;
+	private String domain;
+	private int port;
+	private String password;
+
+	public User(String usern, String dom, int port) {
+		username = usern;
+		domain = dom;
+		this.port = port;
+	}
+
+	public User(Parcel in) {
+		// TODO Auto-generated constructor stub
+		username = in.readString();
+		domain = in.readString();
+		port = in.readInt();
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public int describeContents() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(username);
+		dest.writeString(domain);
+		dest.writeInt(port);
+	}
+
+	public static final Parcelable.Creator<User> CREATOR = new Parcelable.Creator<User>() {
+		public User createFromParcel(Parcel in) {
+			return new User(in);
+		}
+
+		public User[] newArray(int size) {
+			return new User[size];
+		}
+	};
+}
