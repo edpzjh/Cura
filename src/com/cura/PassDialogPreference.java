89049f9968ea2ee8451b76e4e3db3985ef6c2d32
Oday Maleh <oday.maleh@gmail.com>
2012-02-28 20:19:46 +0200

    Preferences for the dialog that appears when choosing to change main Cura password

---
+++ src/com/cura/PassDialogPreference.java
@@ -0,0 +1,88 @@
+/*
+ Copyright© 2010, 2011 Ahmad Balaa, Oday Maleh
+
+ This file is part of Cura.
+
+	Cura is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cura is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cura.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.cura;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.DialogPreference;
+import android.preference.PreferenceManager;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class PassDialogPreference extends DialogPreference {
+	private String oldPassword;
+	// old password to be held
+	private EditText oldPassED, newPassED, confPassED;
+	// 3 editTexts to appear in the prompt for changing the password
+	private SharedPreferences prefs;
+	// to be able to access the content in PreferenceScreen
+	private Context context;
+
+	public PassDialogPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		this.context = context;
+		setDialogLayoutResource(R.layout.prefpassdialog);
+		//
+		prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		oldPassword = prefs.getString("myPass", "");
+	}
+
+	@Override
+	protected void onBindDialogView(View view) {
+		super.onBindDialogView(view);
+		// set the layout of the password dialog
+		oldPassED = (EditText) view.findViewById(R.id.oldPassEditText);
+		newPassED = (EditText) view.findViewById(R.id.newPassEditText);
+		confPassED = (EditText) view.findViewById(R.id.confirmPassEditText);
+	}
+
+	@Override
+	protected void onDialogClosed(boolean positiveResult) {
+		super.onDialogClosed(positiveResult);
+		persistBoolean(positiveResult);
+		if (positiveResult) {
+			//if the fields are set
+			if ((oldPassED.getText().toString()).compareTo(oldPassword) == 0)
+				//and if the old password that was typed in matches the old password
+				if ((newPassED.getText().toString()).compareTo(confPassED
+						.getText().toString()) == 0
+						&& (newPassED.getText().toString()).compareTo("") != 0
+						&& (confPassED.getText().toString()).compareTo("") != 0) {
+					SharedPreferences.Editor editor = prefs.edit();
+					editor.putString("myPass", newPassED.getText().toString());
+					//save the new password
+					editor.commit();
+					Toast.makeText(context, R.string.passwordChanged,
+							Toast.LENGTH_SHORT).show();
+					//show the password changed successfully dialog
+				} else
+					Toast.makeText(context, R.string.wrongConfPass,
+							Toast.LENGTH_SHORT).show();
+				//else show the wrong password dialog
+			else
+				Toast.makeText(context, R.string.wrongPassword,
+						Toast.LENGTH_SHORT).show();
+		}
+
+	}
+
+}
