be6a43f996173a834a93b3dd7bcff326013706ba
Oday Maleh <oday.maleh@gmail.com>
2012-03-03 02:21:44 +0200

    Fixes

--- src/com/cura/sysmonitor/SysMonitorActivity.java
+++ src/com/cura/sysmonitor/SysMonitorActivity.java
@@ -42,33 +42,26 @@ import android.os.RemoteException;
 import android.util.Log;
 import android.widget.Toast;
 
-import com.cura.R;
 import com.cura.Connection.CommunicationInterface;
 import com.cura.Connection.ConnectionService;
 
 public class SysMonitorActivity extends Activity {
 
-	private static TimeSeries timeSeries;
+	private static TimeSeries timeSeriesCPU, timeSeriesRAM;
 	private static XYMultipleSeriesDataset dataset;
 	private static XYMultipleSeriesRenderer renderer;
-	private static XYSeriesRenderer rendererSeries;
+	private static XYSeriesRenderer rendererSeriesCPU, rendererSeriesRAM;
 	private static GraphicalView view;
 
-	// private static Thread mThread;
+	private static Thread mThread;
 
-	private AsyncTask<String, String, String> thread;
 	private CommunicationInterface conn;
-
-	int i = 0;
 
 	private ServiceConnection connection = new ServiceConnection() {
 		public void onServiceConnected(ComponentName arg0, IBinder service) {
 			Log.d("ConnectionService", "Connected");
 			conn = CommunicationInterface.Stub.asInterface(service);
 			// bind to the Connection Service
-			sendAndReceive();
-			// this is the only place we can issue sendAndReceive() and get
-			// output because this is were we are sure that "conn" is not null
 		}
 
 		public void onServiceDisconnected(ComponentName name) {
@@ -82,60 +75,28 @@ public class SysMonitorActivity extends Activity {
 	};
 
 	public synchronized void sendAndReceive() {
-		// now the thread sendAndReceive can go out as an asyncTask instead of a
-		// solitary thread on the main UI thread (good practice)
-		thread = new AsyncTask<String, String, String>() {
-			String res = "";
+		String resultCPU = "";
+		String resultRAM = "";
 
-			@Override
-			protected String doInBackground(String... params) {
-				// this is the second method that AsyncTask invokes
-
-				try {
-					Log.d("Async", "doInBackground");
-					// logs to the LogCat
-					res = conn
-							.executeCommand("ps aux | awk '{ sum+=$3 } END {print sum }'");
-					// this line causes a NullPointerException because "conn" is
-					// null..
-					// it's null because onServiceConnected() has not yet been
-					// called and already the AsyncTask has been executed and
-					// went to the server bas mafi conn..
-					// stores the output of our command in the "res" String
-					return res;
-				} catch (RemoteException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				return "";
+		try {
+			resultCPU = conn
+					.executeCommand("ps aux | awk '{sum+=$3} END {print sum}'");
+			Log.d ("CPUValue", resultCPU);
+			resultRAM = conn
+					.executeCommand("ps aux | awk '{sum+=$4} END {print sum}'");
+			Log.d ("RAMValue", resultRAM);
+			if (!resultCPU.equalsIgnoreCase("")
+					&& !resultRAM.equalsIgnoreCase("")) {
+				// if results are not empty
+				timeSeriesCPU.add(new Date(), Double.parseDouble(resultCPU));
+				timeSeriesRAM.add(new Date(), Double.parseDouble(resultRAM));
+				// add them as points to our graph
+				view.repaint();
 			}
-
-			@Override
-			protected void onPostExecute(String result) {
-				// TODO Auto-generated method stub
-				super.onPostExecute(result);
-				// this is the final method that AsyncTask invokes
-				if (!result.equalsIgnoreCase("")) {
-					// if result is not empty
-					timeSeries.add(new Date(), Double.parseDouble(result));
-					// add it as a point to our graph
-					view.repaint();
-					// repaint the view
-					Log.d("Async", "" + i);
-					i++;
-					// iterate
-				}
-			}
-
-			@Override
-			protected void onPreExecute() {
-				// TODO Auto-generated method stub
-				// this is the first method that AsyncTask invokes
-				super.onPreExecute();
-			}
-		};
-		thread.execute();
-		// run this AsyncTask
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void doBindService() {
@@ -157,50 +118,57 @@ public class SysMonitorActivity extends Activity {
 		renderer.setAxesColor(Color.BLUE);
 		renderer.setAxisTitleTextSize(16);
 		renderer.setChartTitle("System Monitor");
-		renderer.setChartTitleTextSize(15);
-		renderer.setFitLegend(true);
+		renderer.setChartTitleTextSize(25);
+		renderer.setFitLegend(false);
 		renderer.setGridColor(Color.LTGRAY);
 		renderer.setPanEnabled(true, true);
-		renderer.setPointSize(10);
+		renderer.setPointSize(5);
 		renderer.setXTitle("Time");
 		renderer.setYTitle("Number");
 		renderer.setYAxisMax(100);
-		renderer.setMargins(new int[] { 20, 30, 30, 30 });
+		renderer.setMargins(new int[] { 20, 30, 20, 30 });
 		renderer.setZoomButtonsVisible(true);
-		renderer.setBarSpacing(10);
+		renderer.setBarSpacing(20);
+		renderer.setAntialiasing(true);
 		renderer.setShowGrid(true);
 
-		rendererSeries = new XYSeriesRenderer();
-		rendererSeries.setColor(Color.RED);
-		rendererSeries.setFillPoints(true);
-		rendererSeries.setPointStyle(PointStyle.CIRCLE);
+		rendererSeriesCPU = new XYSeriesRenderer();
+		rendererSeriesCPU.setColor(Color.RED);
+		rendererSeriesCPU.setFillPoints(true);
+		rendererSeriesCPU.setPointStyle(PointStyle.CIRCLE);
+		rendererSeriesRAM = new XYSeriesRenderer();
+		rendererSeriesRAM.setColor(Color.GREEN);
+		rendererSeriesRAM.setFillPoints(true);
+		rendererSeriesRAM.setPointStyle(PointStyle.X);
 
-		renderer.addSeriesRenderer(rendererSeries);
+		renderer.addSeriesRenderer(rendererSeriesCPU);
+		renderer.addSeriesRenderer(rendererSeriesRAM);
 
-		timeSeries = new TimeSeries("CPU");
+		timeSeriesCPU = new TimeSeries("CPU");
+		timeSeriesRAM = new TimeSeries("RAM");
 
-		// mThread = new Thread() {
-		// public void run() {
-		// while (true) {
-		// try {
-		// Thread.sleep(1500L);
-		// sendAndReceive();
-		// // view.repaint();
-		// } catch (InterruptedException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// }
-		// }
-		// };
-		// mThread.start();
+		mThread = new Thread() {
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(1500);
+						sendAndReceive();
+					} catch (InterruptedException IE) {
+						IE.printStackTrace();
+					}
+				}
+			}	
+		};
+		mThread.start();
 	}
 
 	@Override
 	protected void onStart() {
 		super.onStart();
-		dataset.addSeries(timeSeries);
-		view = ChartFactory.getTimeChartView(this, dataset, renderer, "Test");
+		dataset.addSeries(timeSeriesCPU);
+		dataset.addSeries(timeSeriesRAM);
+		view = ChartFactory.getTimeChartView(this, dataset, renderer,
+				"Consumption");
 		view.refreshDrawableState();
 		view.repaint();
 		setContentView(view);
