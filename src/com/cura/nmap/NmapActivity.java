2e24cff37708622fe17a910c9156f6fabaeacff6
Oday Maleh <oday.maleh@gmail.com>
2012-04-12 18:17:06 +0300

    License stuff

--- src/com/cura/nmap/NmapActivity.java
+++ src/com/cura/nmap/NmapActivity.java
@@ -1,4 +1,21 @@
-package com.cura.nmap;
+/*
+ Copyright© 2010, 2011 WJHolden
+
+ This file is part of Cura.
+
+	Cura is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Cura is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Cura.  If not, see <http://www.gnu.org/licenses/>.
+ */package com.cura.nmap;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
