067d7adfc0fa243955a2bab85b1f071030e0e31c
Oday Maleh <oday.maleh@gmail.com>
2012-07-27 04:52:44 +0300

    Version updated in code

--- src/com/cura/about/AboutClass.java
+++ src/com/cura/about/AboutClass.java
@@ -27,20 +27,24 @@ package com.cura.about;
 public class AboutClass {
 	private String title;
 	private String subtitle;
-	
+
 	public AboutClass(String title, String subtitle) {
 		this.title = title;
 		this.subtitle = subtitle;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getSubtitle() {
 		return subtitle;
 	}
+
 	public void setSubtitle(String subtitle) {
 		this.subtitle = subtitle;
 	}
